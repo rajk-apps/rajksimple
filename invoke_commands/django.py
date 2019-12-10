b8ea91c7798ec9af719b37fff1f2cfb0f1b5de1d
Endre Márk Borza <endremborza@gmail.com>
2019-12-10 22:34:01 +0100

    update django boiler command

--- invoke_commands/django.py
+++ invoke_commands/django.py
@@ -14,3 +14,12 @@ def setup_dev(c):
 @task
 def clean(c):
     c.run("rm -{}/migrations/*".format(package_name))
+
+
+@task
+def update_boilerplate(c, drop=False):
+    c.run("git fetch boilerplate")
+    c.run("git merge boilerplate/django-app --no-edit")
+    if drop:
+        c.run("rm -rf mydjangoapp")
+        c.run("rm -rf mypackage")
