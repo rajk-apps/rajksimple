2461d8acb140b8a4281039d294e3d1b2201839d3
Endre Márk Borza <endremborza@gmail.com>
2019-12-19 20:43:12 +0100

    notebook task

--- tasks.py
+++ tasks.py
@@ -10,10 +10,17 @@ def lint(c):
     c.run("black {}".format(package_name))
     c.run("flake8 {}".format(package_name))
 
+
 @task
 def update_boilerplate(c):
     c.run("git fetch boilerplate")  # TODO: this knows the name of the remote
     c.run("git merge boilerplate/{} --no-edit".format(boilerplate_branch))
+
+
+@task
+def notebook(c):
+    c.run("export PATH=$PATH:..")
+    c.run("jupyter-notebook")
 
 
 ns = Collection()
