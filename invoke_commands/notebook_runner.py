f0e157afa2f7a570a648966ce1968a7a86d48e74
Endre Márk Borza <endremborza@gmail.com>
2020-01-10 23:12:00 +0100

    merge relevant task updates from master

---
+++ invoke_commands/notebook_runner.py
@@ -0,0 +1,14 @@
+from jupyter_client.kernelspec import KernelSpecManager
+from notebook.notebookapp import main
+
+
+class MyManager(KernelSpecManager):
+
+    def get_kernel_spec(self, kernel_name):
+        init_resp = super().get_kernel_spec(kernel_name)
+        init_resp.argv = [*init_resp.argv, """--IPKernelApp.exec_lines=['import sys', 'sys.path.append("..")']"""]
+        return init_resp
+
+
+if __name__ == "__main__":
+    main(kernel_spec_manager_class=MyManager)
--- tasks.py
+++ tasks.py
@@ -1,14 +1,6 @@
-from invoke import task, Collection
+from invoke import Collection
 
-from invoke_commands import docs, clean, sonar, test, release, django
-
-from invoke_commands.vars import package_name
-
-
-@task
-def lint(c):
-    c.run("black {}".format(package_name))
-    c.run("flake8 {}".format(package_name))
+from invoke_commands import docs, clean, sonar, test, release, misc, django
 
 
 ns = Collection()
@@ -17,4 +9,5 @@ ns.add_collection(Collection.from_module(docs))
 ns.add_collection(Collection.from_module(clean))
 ns.add_collection(Collection.from_module(sonar))
 ns.add_collection(Collection.from_module(test))
+ns.add_collection(Collection.from_module(misc))
 ns.add_collection(Collection.from_module(django))
