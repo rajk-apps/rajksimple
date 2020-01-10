87e9c600d67b612a37fec7f73618bfe70ed46dd8
Endre Márk Borza <endremborza@gmail.com>
2020-01-10 23:13:56 +0100

    delete mypackage

--- mypackage/__init__.py
+++
@@ -1,2 +0,0 @@
-from ._version import __version__  # noqa: F401
-from .core import add_two  # noqa: F401
--- mypackage/core/__init__.py
+++
@@ -1,6 +0,0 @@
-def add_two(x: int):
-    """add 2 to a number
-
-    thi is a sample function
-    for the template repo"""
-    return x + 2
